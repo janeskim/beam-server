f12c7e3787057e4142488f702def5146ae6b8c0e
janeskim <kim.jane.sue@gmail.com>
2019-08-16 17:31:30 -0500

    Setup basic application factory

---
+++ src/__init__.py
@@ -0,0 +1,32 @@
+""" Application factory """
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    """ Function to create app instance. """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'code.sqlite'),
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        """ Test route to see if application factory works. """
+        return 'Hello, World!'
+
+    return app
